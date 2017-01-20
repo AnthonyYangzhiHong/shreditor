00eab3348f8c8c057863fabded782035d85c63c0
shrimp <amoy0226@gmail.com>
2017-01-20 19:24:43 +0800

    路径修改

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ var app = express();
 
 app.use(express.static('static'));
 
-app.get('/index', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './index.html'))
 });
 
